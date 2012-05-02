12e91f5957b1a016b02718cf7e605358cdba3dfc
robkaram <rak65@case.edu>
2012-05-02 17:46:20 -0400

    basic UI (pin entry) + downloading/display of GB data

--- src/com/bryanmarty/greenbutton/GreenButtonProjectActivity.java
+++ src/com/bryanmarty/greenbutton/GreenButtonProjectActivity.java
@@ -2,6 +2,11 @@ package com.bryanmarty.greenbutton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import java.net.*;
+import java.io.*;
 
 public class GreenButtonProjectActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,41 @@ public class GreenButtonProjectActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+    }
+    
+    public void getGBData(View view) throws IOException
+    {
+    	// TODO: probably should  download on separate thread & show progress spinner somewhere
+    	EditText txtPin = (EditText)this.findViewById(R.id.txtPIN_Entry);
+    	String pin = txtPin.getText().toString();
+    	if(pin.equals("") || pin.length() != 7)
+    	{
+    		Toast.makeText(getApplicationContext(), "Invalid PIN, please enter again.", Toast.LENGTH_SHORT).show();
+    		return;
+    	}
+    	URL url = new URL("http://greenbutton.case.edu/get/" + pin);
+    	HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    	try 
+    	{
+    		InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+    		BufferedReader gbReader = new BufferedReader(new InputStreamReader(in));
+    	     String gbData = "";
+    	     StringBuffer gbBuffer = new StringBuffer();
+    	     while((gbData = gbReader.readLine()) != null)
+    	     {
+    	    	 gbBuffer.append(gbData + System.getProperty("line.separator"));
+    	     }
+    	     EditText txtGBData = (EditText)this.findViewById(R.id.txtGBDataResponse);
+    	     txtGBData.setText(gbBuffer);
+    	}
+    	catch (IOException ioe) {
+    		// we might consider having a special failure response instead of catching the exception
+    		Toast.makeText(getApplicationContext(), "Invalid PIN, please enter again.", Toast.LENGTH_SHORT).show();
+    	}
+    	finally 
+    	{
+    	     urlConnection.disconnect();
+    	}
     }
 }
