9c82052422adca1bd88acfad3b5e2f9ec7856b50
robkaram <rak65@case.edu>
2012-05-03 19:04:12 -0400

    Added private class to main activity extending AsyncTask Downloads GB data on background thread, plays spinning progress animation next to button.

--- src/com/bryanmarty/greenbutton/GreenButtonProjectActivity.java
+++ src/com/bryanmarty/greenbutton/GreenButtonProjectActivity.java
@@ -1,6 +1,7 @@
 package com.bryanmarty.greenbutton;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -9,17 +10,25 @@ import java.net.*;
 import java.io.*;
 
 public class GreenButtonProjectActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+	private EditText txtGBData;
+	private Button btnGetGBData;
+	private ProgressBar pbGBDownloadProgress;
+	
+	/** Called when the activity is first created. */	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        txtGBData = (EditText)this.findViewById(R.id.txtGBDataResponse);
+        btnGetGBData = (Button)this.findViewById(R.id.btnGetGBData);
+        pbGBDownloadProgress = (ProgressBar)this.findViewById(R.id.gbDownloadProgress);
     }
     
     public void getGBData(View view) throws IOException
     {
-    	// TODO: probably should  download on separate thread & show progress spinner somewhere
+    	GBDataDownloader gbDataDownloader = new GBDataDownloader();
     	EditText txtPin = (EditText)this.findViewById(R.id.txtPIN_Entry);
     	String pin = txtPin.getText().toString();
     	if(pin.equals("") || pin.length() != 7)
@@ -27,28 +36,65 @@ public class GreenButtonProjectActivity extends Activity {
     		Toast.makeText(getApplicationContext(), "Invalid PIN, please enter again.", Toast.LENGTH_SHORT).show();
     		return;
     	}
-    	URL url = new URL("http://greenbutton.case.edu/get/" + pin);
-    	HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-    	try 
+    	
+    	gbDataDownloader.execute(pin);
+    	
+    }
+    
+    private class GBDataDownloader extends AsyncTask<String, Integer, String> 
+    {    
+    	protected void onPreExecute() 
     	{
-    		InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-    		BufferedReader gbReader = new BufferedReader(new InputStreamReader(in));
-    	     String gbData = "";
-    	     StringBuffer gbBuffer = new StringBuffer();
-    	     while((gbData = gbReader.readLine()) != null)
-    	     {
-    	    	 gbBuffer.append(gbData + System.getProperty("line.separator"));
-    	     }
-    	     EditText txtGBData = (EditText)this.findViewById(R.id.txtGBDataResponse);
-    	     txtGBData.setText(gbBuffer);
+    		btnGetGBData.setEnabled(false);
+    		pbGBDownloadProgress.setVisibility(View.VISIBLE);
+    		
     	}
-    	catch (IOException ioe) {
-    		// we might consider having a special failure response instead of catching the exception
-    		Toast.makeText(getApplicationContext(), "Invalid PIN, please enter again.", Toast.LENGTH_SHORT).show();
-    	}
-    	finally 
+    	
+    	protected String doInBackground(String... pin) 
     	{
-    	     urlConnection.disconnect();
-    	}
+    		URL url;
+    		HttpURLConnection urlConnection = null; 
+    		String gbData = "";
+    		StringBuffer gbBuffer = null;
+    		
+    		try 
+        	{
+    			url = new URL("http://greenbutton.case.edu/get/" + pin[0]);
+    			urlConnection = (HttpURLConnection) url.openConnection();
+            	
+        		InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        		BufferedReader gbReader = new BufferedReader(new InputStreamReader(in));
+        	     gbBuffer = new StringBuffer();
+        	     while((gbData = gbReader.readLine()) != null)
+        	     {
+        	    	 gbBuffer.append(gbData + System.getProperty("line.separator"));
+        	     }
+        	}
+        	catch (IOException ioe) {
+        		// we might consider having a special failure response instead of catching the exception
+        		//Toast.makeText(getApplicationContext(), "Invalid PIN, please enter again.", Toast.LENGTH_SHORT).show();
+        	}
+        	finally
+        	{
+        		if(urlConnection != null)
+        			urlConnection.disconnect();
+        		 //view.setEnabled(true);
+        	     //pb.setVisibility(View.GONE);
+        	}
+    		
+    		return gbBuffer != null ? gbBuffer.toString() : "Error downloading Green Button from server\n"; // TODO: more intelligent error message
+    	    
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        	
+        }
+
+        protected void onPostExecute(String result) 
+        {
+        	btnGetGBData.setEnabled(true);
+        	pbGBDownloadProgress.setVisibility(View.GONE);
+        	txtGBData.setText(result);
+        }
     }
 }
