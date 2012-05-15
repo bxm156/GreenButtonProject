b549c3360beec23ade4329f967f40c51d3d8e37f
robkaram <rak65@case.edu>
2012-05-15 19:21:29 -0400

    added days remaining to budget

--- src/com/bryanmarty/greenbutton/GreenButtonBudgetActivity.java
+++ src/com/bryanmarty/greenbutton/GreenButtonBudgetActivity.java
@@ -1,9 +1,11 @@
 package com.bryanmarty.greenbutton;
 
 import java.net.ContentHandler;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GreenButtonBudgetActivity extends Activity {
 
@@ -13,11 +15,33 @@ public class GreenButtonBudgetActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	
 	    setContentView(R.layout.budget);
+	    
+	    setUpDate();
 	}
 
 	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
+	protected void onStart() {		
 		super.onStart();
+		
+	}
+	
+	protected void setUpDate()
+	{
+		TextView txtDaysRemaining = (TextView)this.findViewById(R.id.txtDaysRemaining);
+		Calendar c = Calendar.getInstance();
+		int daysInMonth = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int dayInMonth = c.get(Calendar.DAY_OF_MONTH);
+		int daysLeft = daysInMonth - dayInMonth;
+		txtDaysRemaining.setText("" + daysLeft);
+	}
+	
+	protected void setUpUsageCurrent()
+	{
+		
+	}
+	
+	protected void setUpUsagePrevious()
+	{
+		
 	}
 }
