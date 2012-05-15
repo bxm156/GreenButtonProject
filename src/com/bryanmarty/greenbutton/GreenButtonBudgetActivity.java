fbbc51a1b1cec795c30c31979609fa87774b633e
robkaram <rak65@case.edu>
2012-05-15 19:53:53 -0400

    made more progress on budget

--- src/com/bryanmarty/greenbutton/GreenButtonBudgetActivity.java
+++ src/com/bryanmarty/greenbutton/GreenButtonBudgetActivity.java
@@ -2,6 +2,12 @@ package com.bryanmarty.greenbutton;
 
 import java.net.ContentHandler;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.concurrent.Future;
+
+import com.bryanmarty.greenbutton.data.IntervalReading;
+import com.bryanmarty.greenbutton.database.TrackManager;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -37,7 +43,41 @@ public class GreenButtonBudgetActivity extends Activity {
 	
 	protected void setUpUsageCurrent()
 	{
+		LinkedList<IntervalReading> result = new LinkedList<IntervalReading>();
 		
+		Calendar c = Calendar.getInstance();
+		c.roll(Calendar.MONTH, -1); // last month
+		c.set(Calendar.DAY_OF_MONTH, 1);
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+		
+		Date startLastMonth = c.getTime();
+		c.roll(Calendar.DAY_OF_MONTH, c.getActualMaximum(Calendar.DAY_OF_MONTH));
+		Date endLastMonth = c.getTime();
+		
+		Future<LinkedList<IntervalReading>> future = TrackManager.getReadingsBetween(startLastMonth, endLastMonth);
+		try {
+			result = future.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		int usagePrevious = 0;
+		for(IntervalReading r : result)
+		{
+			usagePrevious += r.getValue();
+		}
+		
+		Calendar c2 = Calendar.getInstance();
+		c2.set(Calendar.DAY_OF_MONTH, 1);
+		c2.set(Calendar.HOUR_OF_DAY, 0);
+		c2.set(Calendar.MINUTE, 0);
+		c2.set(Calendar.SECOND, 0);
+		c2.set(Calendar.MILLISECOND, 0);
+		Date now = c2.getTime();
+		
+		Future<LinkedList<IntervalReading>> future2 = TrackManager.getReadingsSince(now);
 	}
 	
 	protected void setUpUsagePrevious()
