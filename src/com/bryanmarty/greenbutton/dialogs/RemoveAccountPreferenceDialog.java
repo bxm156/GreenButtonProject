f33e794eb5025cccf8b6e73e36d72fb4ea7580f9
Bryan Marty <bxm156@case.edu>
2012-08-31 18:24:44 -0400

    Another preference item

---
+++ src/com/bryanmarty/greenbutton/dialogs/RemoveAccountPreferenceDialog.java
@@ -0,0 +1,19 @@
+package com.bryanmarty.greenbutton.dialogs;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class RemoveAccountPreferenceDialog extends DialogPreference {
+
+	public RemoveAccountPreferenceDialog(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RemoveAccountPreferenceDialog(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+}
