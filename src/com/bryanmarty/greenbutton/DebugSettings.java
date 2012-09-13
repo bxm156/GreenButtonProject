1917b8b02d2d47dba99418d3cd4d41df8911ff1b
Bryan Marty <bxm156@case.edu>
2012-09-12 22:22:34 -0400

    Updated server to new budgetityourself.com domain

--- src/com/bryanmarty/greenbutton/DebugSettings.java
+++ src/com/bryanmarty/greenbutton/DebugSettings.java
@@ -3,5 +3,5 @@ package com.bryanmarty.greenbutton;
 public class DebugSettings {
 	public static final boolean DEBUG = false;
 	public static final boolean ALWAYSNEWDATABASE = false;
-	public static final String SERVER = "http://greenbutton.case.edu/";
+	public static final String SERVER = "http://www.budgetityourself.com/";
 }
