1ac41c5fb597f7435264407c17d3a91dd7aa8156
robkaram <rak65@case.edu>
2012-10-14 23:21:51 -0400

    made the textview with the URL work as a link, opens up the BIY website.
    tweaks to text size in about page.

--- src/com/bryanmarty/greenbutton/GreenButtonAboutActivity.java
+++ src/com/bryanmarty/greenbutton/GreenButtonAboutActivity.java
@@ -2,6 +2,8 @@ package com.bryanmarty.greenbutton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class GreenButtonAboutActivity extends Activity {
 
@@ -9,6 +11,9 @@ public class GreenButtonAboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
 	    setContentView(R.layout.about);
+	    
+	    TextView link = (TextView)findViewById(R.id.textAboutLink);
+	    link.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 
 }
